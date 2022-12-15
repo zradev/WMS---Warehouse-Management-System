dbe108a6294f6ad50f6f6bb56a8161e91c0aaf63
Zlatko Radev <zlatkoradevv@gmail.com>
2022-12-16 00:44:17 +0200

    Add Main Layout Component

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -24,8 +24,11 @@ const Signup = () => {
   });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div className="flex flex-col w-1/2 space-y-5">
+    <form
+      onSubmit={formik.handleSubmit}
+      className="flex w-full justify-center items-center my-5"
+    >
+      <div className="flex flex-col space-y-5 w-full sm:w-2/3 md:w-1/3 mx-5">
         <label htmlFor="username" className="text-start font-bold">
           Username<span className="text-red-800">*</span>
         </label>
@@ -35,7 +38,7 @@ const Signup = () => {
           id="username"
           onChange={formik.handleChange}
           value={formik.values.username}
-          className="border border-black"
+          className="rounded border border-gray-600 focus:border-black"
         />
         <label htmlFor="password" className="text-start font-bold">
           Password<span className="text-red-800">*</span>
@@ -46,7 +49,7 @@ const Signup = () => {
           id="password"
           onChange={formik.handleChange}
           value={formik.values.password}
-          className="border border-black"
+          className="rounded border border-gray-600 focus:border-black"
         />
         <label htmlFor="email" className="text-start font-bold">
           Email<span className="text-red-800">*</span>
@@ -57,7 +60,7 @@ const Signup = () => {
           id="email"
           onChange={formik.handleChange}
           value={formik.values.email}
-          className="border border-black"
+          className="rounded border border-gray-600 focus:border-black"
         />
         <label htmlFor="phone" className="text-start font-bold">
           Phone
@@ -70,7 +73,12 @@ const Signup = () => {
           value={formik.values.phone}
           className="rounded border border-gray-600 focus:border-black"
         />
-        <button type="submit">Signup</button>
+        <button
+          type="submit"
+          className="w-fit self-center p-1 px-6 rounded-lg  border border-black hover:bg-gray-100 font-bold"
+        >
+          Register
+        </button>
       </div>
     </form>
   );
