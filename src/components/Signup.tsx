9ecf6559b489d8dce96a99de53d667f7aa7eb9c4
Zlatko Radev <zlatkoradevv@gmail.com>
2022-12-18 22:57:45 +0200

    Refactor Signup Component

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Signup = () => {
@@ -15,7 +15,10 @@ const Signup = () => {
     onSubmit: async (values) => {
       console.log(values);
       try {
-        //await axios.post(`${process.env.REACT_APP_SERVER_URL}/user/register`, values);
+        await axios.post(
+          `${process.env.REACT_APP_SERVER_URL}/user/register`,
+          values
+        );
         navigate("/");
       } catch (error) {
         console.log(error);
@@ -26,7 +29,7 @@ const Signup = () => {
   return (
     <form
       onSubmit={formik.handleSubmit}
-      className="flex w-full justify-center items-center my-5"
+      className="flex w-full justify-center items-center my-10"
     >
       <div className="flex flex-col space-y-5 w-full sm:w-2/3 md:w-1/3 mx-5">
         <label htmlFor="username" className="text-start font-bold">
@@ -36,6 +39,7 @@ const Signup = () => {
           type="text"
           name="username"
           id="username"
+          required
           onChange={formik.handleChange}
           value={formik.values.username}
           className="rounded border border-gray-600 focus:border-black"
@@ -47,6 +51,7 @@ const Signup = () => {
           type="password"
           name="password"
           id="password"
+          required
           onChange={formik.handleChange}
           value={formik.values.password}
           className="rounded border border-gray-600 focus:border-black"
@@ -58,6 +63,7 @@ const Signup = () => {
           type="email"
           name="email"
           id="email"
+          required
           onChange={formik.handleChange}
           value={formik.values.email}
           className="rounded border border-gray-600 focus:border-black"
@@ -66,19 +72,33 @@ const Signup = () => {
           Phone
         </label>
         <input
-          type="phone"
+          type="tel"
           name="phone"
           id="phone"
+          pattern="^[0-9\s-]{3,15}$"
           onChange={formik.handleChange}
           value={formik.values.phone}
           className="rounded border border-gray-600 focus:border-black"
         />
+        <div className="flex space-x-8">
+          <input type="checkbox" name="checkbox" id="checkbox" required />
+          <label htmlFor="checkbox" className="text-xs">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+            repudiandae similique quo ea consequuntur molestias ab.
+          </label>
+        </div>
         <button
           type="submit"
           className="w-fit self-center p-1 px-6 rounded-lg  border border-black hover:bg-gray-100 font-bold"
         >
           Register
         </button>
+        <p className="mt-3">
+          Already have an account?{" "}
+          <Link to="/" className="text-blue-400 hover:text-blue-600">
+            Log In
+          </Link>
+        </p>
       </div>
     </form>
   );
