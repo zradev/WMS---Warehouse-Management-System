fcd52fdf8283eed546d4b01cd16670f1e730586e
zradev <zlatkoradevv@gmail.com>
2022-12-26 14:48:49 +0200

    Refactor Sing In and Sing Up components

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,9 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import AuthContext from "../context/AuthProvider";
 import { useFormik } from "formik";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { axiosErrorHandler } from "../utils/axiosErrorHandler";
 
 const Signin = () => {
+  const { login } = useContext(AuthContext);
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   const formik = useFormik({
     initialValues: {
@@ -17,10 +21,12 @@ const Signin = () => {
           `${process.env.REACT_APP_SERVER_URL}/user/login`,
           values
         );
-        localStorage.setItem("user", response.data);
-        navigate("/");
+        const jwt = response?.data?.token;
+        login(jwt);
+        navigate("/products");
       } catch (error) {
         console.log(error);
+        setError(axiosErrorHandler(error));
       }
     },
   });
@@ -33,6 +39,7 @@ const Signin = () => {
       className="flex w-full justify-center items-center my-10"
     >
       <div className="flex flex-col space-y-5 w-full sm:w-2/3 md:w-1/3 mx-5">
+        <p className="text-center text-red-600">{error}</p>
         <label htmlFor="username" className="text-start font-bold">
           Username<span className="text-red-800">*</span>
         </label>
