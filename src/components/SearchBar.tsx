34ec55082179a093d8e7ab236aeae1e05535b052
zradev <zlatkoradevv@gmail.com>
2022-12-31 18:09:19 +0200

    Refactor SearchBar Component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,27 @@
-export default function SearchComponent() {
+import React, { useState } from "react";
+
+interface IProps {
+  setSearchParam: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const SearchBar = ({ setSearchParam }: IProps) => {
+  const [param, setParam] = useState("");
   return (
     <div className="flex items-center mx-2 md:mx-5">
-      <div className="flex space-x-1">
+      <form onSubmit={(e) => e.preventDefault()} className="flex space-x-1">
         <input
           type="text"
+          onChange={(e) => {
+            setParam(e.target.value);
+            setTimeout(() => setSearchParam(e.target.value), 1500);
+          }}
           className="block w-full px-4 py-2 text-stone-700 bg-white border border-stone-300 rounded-full focus:border-stone-400 focus:ring-stone-300 focus:outline-none focus:ring focus:ring-opacity-40"
           placeholder="Search..."
         />
-        <button className="px-4 text-white bg-stone-700 rounded-full ">
+        <button
+          className="px-4 text-white bg-stone-700 rounded-full "
+          onClick={() => setSearchParam(param)}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="w-5 h-5"
@@ -23,7 +37,9 @@ export default function SearchComponent() {
             />
           </svg>
         </button>
-      </div>
+      </form>
     </div>
   );
-}
+};
+
+export default SearchBar;
